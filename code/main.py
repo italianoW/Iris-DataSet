66cba495b90e7fba608fe4533df964a5141c7fef
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-07-15 19:48:50 -0300

    feat(main): change main code to a function

--- code/main.py
+++ code/main.py
@@ -1,11 +1,10 @@
 import treino
 import util
 
-
-
-if __name__ == "__main__":
-    
+def main():
     dataset = util.download_dados()
     dados_de_treino, dados_de_teste = util.divisao_treino_teste(dataset)
     modelo = treino.treinar(dados_de_treino)
-    
+
+if __name__ == "__main__":
+    main()

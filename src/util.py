6b682ec75db5b29f3f4ad6ea58f74b865c290853
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-07-17 22:22:39 -0300

    feat(pylint): finish clean util

--- src/util.py
+++ src/util.py
@@ -1,52 +1,63 @@
-import pandas as pd
-import kagglehub
-import os
+"""CHANGE!!!"""
+
 import math
+import os
+import pandas as pd
 
-
-def download_dados(): #returns data from iris_dataset 
-
-    return pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", "model_data", "iris.data.csv"))
+CSV = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", "model_data", "iris.data.csv")
+def download_dados():
+    """CHANGE!!!"""
+    return pd.read_csv(CSV)
 
 def divisao_treino_teste(dados):
+    """CHANGE!!!"""
 
     lista = dados.values.tolist()
-    tamanho = len(lista)
     treino,teste = [], []
     atributos_teste, rotulos_teste, atributos_treino, rotulos_treino = [], [], [], []
-
-        #lista[0], lista[48]
-        #lista[49],lista[98])
-        #lista[99],lista[148])
-
     lista.insert(0,lista[0])
 
     for i in range(3):
         for j in range(33):
             treino.append(lista[i*50 + j])
-    
+
     for i in range(3):
         for j in range(17):
             teste.append(lista[i*50 + 33 + j])
 
     for i in range(99):
         atributos_treino.append(treino[i][:4])
-        rotulos_treino.append(0 if lista[i][4]== "Iris-setosa" else 1 if lista[i][4]== "Iris-versicolor" else 2) 
+
+        if lista[i][4]== "Iris-setosa":
+            rotulos_treino.append(0)
+        elif lista[i][4]== "Iris-versicolor":
+            rotulos_treino.append(1)
+        else:
+            rotulos_treino.append(2)
 
     for i in range(51):
         atributos_teste.append(treino[i][:4])
-        rotulos_teste.append(0 if lista[i][4]== "Iris-setosa" else 1 if lista[i][4]== "Iris-versicolor" else 2) 
+
+        if lista[i][4]== "Iris-setosa":
+            rotulos_teste.append(0)
+        elif lista[i][4]== "Iris-versicolor":
+            rotulos_teste.append(1)
+        else:
+            rotulos_teste.append(2)
 
     return atributos_teste,rotulos_teste,atributos_treino,rotulos_treino
 
 def mse(saida, esperado):
-        """MSE."""
-        return sum((s - e) ** 2 for s, e in zip(saida, esperado)) / len(saida)
+    """CHANGE!!!"""
+    return sum((s - e) ** 2 for s, e in zip(saida, esperado)) / len(saida)
 
 def avaliar(bias, modelo, atributos_teste, rotulos_teste):
+    """CHANGE!!!"""
     acertos = 0
     for atributos, rotulo in zip(atributos_teste, rotulos_teste):
-        saida = relu([bias[i] + sum(atributos[j] * modelo[j][i] for j in range(4)) for i in range(3)])
+        saida = relu([bias[i] + sum(atributos[j] * modelo[j][i]
+                                    for j in range(4))
+                      for i in range(3)])
         saida = softmax(saida)
         pred = saida.index(max(saida))
         if pred == rotulo:
@@ -55,10 +66,12 @@ def avaliar(bias, modelo, atributos_teste, rotulos_teste):
     return accuracy, bias
 
 def softmax(x):
+    """CHANGE!!!"""
     max_x = max(x)
-    e_x = [math.exp(i - max_x) for i in x]  # shift to prevent overflow
+    e_x = [math.exp(i - max_x) for i in x]
     soma = sum(e_x)
     return [i / soma for i in e_x]
 
 def relu(x):
+    """CHANGE!!!"""
     return [max(0,v) for v in x]
